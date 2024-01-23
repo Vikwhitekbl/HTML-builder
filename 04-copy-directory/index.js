217c7b2a831d7218bfa5a51980c3eba04c0da8a3
Viktor Belov <vbelov@vansofts.net>
2024-01-23 17:18:20 +0300

    fix: 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,13 +9,26 @@ const copyDir = async () => {
         await fs.promises.mkdir(newDirPath, { recursive: true });
 
         const files = await fs.promises.readdir(dirPath);
+        const copiedList = [];
+
         for (let file of files) {
             const filePath = path.join(dirPath, file);
             const newFilePath = path.join(newDirPath, file);
 
             const inf = await fs.promises.lstat(filePath);
             if (inf.isFile()) {
-                await fs.promises.copyFile(filePath,newFilePath);
+                await fs.promises.copyFile(filePath, newFilePath);
+                copiedList.push(file.toLowerCase());
+            }
+        }
+        const newCopiedList = copiedList.map((file) => file.toLowerCase());
+        const copiedFiles = await fs.promises.readdir(newDirPath);
+
+        for (let file of copiedFiles) {
+            const fileName = file.toLowerCase();
+            if (!newCopiedList.includes(fileName)) {
+                const newFilePath = path.join(newDirPath, file);
+                await fs.promises.unlink(newFilePath);
             }
         }
         console.log('All files successfully copied.');
